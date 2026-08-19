aef6d42c22141b90e5347c2423f3b00b045c33f0
Alta3ees <118279375+Alta3ees@users.noreply.github.com>
2026-08-19 16:21:16 +0200

    Apply final v0.4 UI cleanup overrides

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import ThemeDock from "./ThemeDock";
 import "./styles.css";
 import "./actions.css";
 import "./protein-theme.css";
+import "./ui-fixes.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
